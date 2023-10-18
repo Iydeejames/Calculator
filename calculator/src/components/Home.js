a831afad322a5d56c94e6b8497113bc31fbc8bae
Idaraobong James <iydeejaymes@gmail.com>
2023-10-18 11:26:30 +0100

    Buttons component

--- calculator/src/components/Home.js
+++ calculator/src/components/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import classes from "./Home.module.css"
-import Demo from './Demo'
+import Button from "./Button"
 
 function Home() {
+
+  const buttons = ["C", "9", "/", "8", "7", "6", "5", "4", "3",
+   "+", "2", "1", "0", "-", ".", "Del", "="]
+
   return (
     <div className={classes.home}>
       <div className={classes.inner}>
@@ -11,7 +15,8 @@ function Home() {
         </div>
       </div>
       <div className={classes.btns}>
-
+      {buttons.map((ele,index) => {return <Button value={ele}
+      key={index}/>})}
       </div>
     </div>
   )
