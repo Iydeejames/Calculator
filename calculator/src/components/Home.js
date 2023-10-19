52eac87545a8f98706413e59f57fc67c4db3b4fd
Idaraobong James <iydeejaymes@gmail.com>
2023-10-19 17:36:41 +0100

    Nothing much

--- calculator/src/components/Home.js
+++ calculator/src/components/Home.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import classes from "./Home.module.css"
-import Button from "./Button"
+import Button from './Button'
 
-function Home() {
+function Home(){
 
   const buttons = ["C", "9", "/", "8", "7", "6", "5", "4", "3",
    "+", "2", "1", "0", "-", ".", "Del", "="]
